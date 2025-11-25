014760c98687ee625b2b2bf78c86dc6a426279cb
hashitha040 <harshithatenali11@gmail.com>
2025-11-25 23:28:29 +0530

    movie rating

--- moviereview/moviereview/urls.py
+++ moviereview/moviereview/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from review.views import basic,movie_info
+from review.views import basic,movie_info,movies
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('basic/', basic),
     path('movie/',movie_info),
+    path('movies/',movies),
 ]
--- moviereview/review/views.py
+++ moviereview/review/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
+import json
+from review.models import Movie_details
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 def basic(request):
@@ -10,3 +13,15 @@ def movie_info(request):
     movie=request.GET.get('movie')
     date=request.GET.get('date')
     return JsonResponse({'status':"success",'result':{'movie_name':movie,'release_date':date}},status=200)
+
+
+@csrf_exempt
+def movies(request):
+    if request.method=='POST':
+        data=json.loads(request.body)
+        movie=Movie_details.objects.create(movie_name=data.get("movie_name"),release_date=data.get("release_date"),budget=data.get("budget"),rating=data.get("rating"))
+        return JsonResponse({'status':"success",'message':"Movie record inserted successfully","data":data},status=200)
+    return JsonResponse({'error':"error occured"},status=400)
+
+
+
